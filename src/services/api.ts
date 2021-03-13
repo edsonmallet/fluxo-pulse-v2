69b83f3722e7f09811f5b26a4ec406fa08fc7316
Edson Mallet <edsonmmr@gmail.com>
2021-03-13 17:56:32 -0300

    feat: add url to production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:3344/api/v1'
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://127.0.0.1:3344/api/v1'
+      : 'http://dev-app-fluxo-live.herokuapp.com/api/v1'
 })
 
 export default api
